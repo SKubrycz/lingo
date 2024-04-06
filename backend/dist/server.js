ae3d93624e520beb968cfa2ed3dae185893866a4
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-06 12:11:06 +0200

    Add files via upload

---
+++ backend/dist/server.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require('express');
+const app = express();
+const port = 8000;
+app.listen(port);
+console.log('Something from the server');
+console.log('restart?');
+//# sourceMappingURL=server.js.map
