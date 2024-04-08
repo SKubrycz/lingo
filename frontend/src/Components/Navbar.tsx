d2bced407ec3c7f61b4a6b4b176f33536994c569
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-08 15:58:30 +0200

    Update Navbar.tsx
    added link to Home through LOGO element

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function NavHome() {
     return (
         <nav className='home-navbar'>
-            <div className='home-navbar-logo'>LOGO</div>
+            <Link to='/' className='home-navbar-logo'>LOGO</Link>
             <Link to='/about'>O nas</Link>
             <Link to='/login'>Logowanie</Link>
             <Link to='/register'>Rejestracja</Link>
