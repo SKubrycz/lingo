4d97101246a1f3cdf7f1a9822a2190fa2e73f280
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-11 11:12:44 +0200

    Update Login.tsx
    added login form

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -6,6 +6,20 @@ function Login() {
 
     return (
         <>
+            <div className='wrapper'>
+                <div className='main-login-wrapper'>
+                    <main className='main-login'>
+                        <h2>Zaloguj się</h2>
+                        <form className='login-form'>
+                            {/* <label>Nazwa użytkownika</label> */}
+                            <input type='login' placeholder='Nazwa użytkownika'></input>
+                            {/* <label>Hasło</label> */}
+                            <input type='password' placeholder='Hasło'></input>
+                            <input type='submit' value='Zaloguj'></input>
+                        </form>
+                    </main>
+                </div>
+            </div>
         </>
     );
 }
