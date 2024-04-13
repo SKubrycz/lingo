0aec9144d67624d7e090ac227145dceee1534bf3
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-13 19:46:02 +0200

    Update Login.tsx
    created MainLogin Component

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -1,4 +1,5 @@
 import NavLogin from './NavLogin';
+import MainLogin from './MainLogin';
 
 import './Login.scss';
 
@@ -9,18 +10,7 @@ function Login() {
         <>
             <div className='wrapper'>
                 <NavLogin></NavLogin>
-                <div className='main-login-wrapper'>
-                    <main className='main-login'>
-                        <h2>Zaloguj się</h2>
-                        <form className='login-form'>
-                            {/* <label>Nazwa użytkownika</label> */}
-                            <input type='login' placeholder='Nazwa użytkownika'></input>
-                            {/* <label>Hasło</label> */}
-                            <input type='password' placeholder='Hasło'></input>
-                            <input type='submit' value='Zaloguj'></input>
-                        </form>
-                    </main>
-                </div>
+                <MainLogin></MainLogin>
             </div>
         </>
     );
