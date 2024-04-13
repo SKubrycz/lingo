df091837e399053e3a4d7aaca9e22366a58d77e7
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-13 19:58:43 +0200

    Add files via upload

---
+++ frontend/src/Components/About/MainAbout.tsx
@@ -0,0 +1,17 @@
+
+
+function MainAbout() {
+
+    return (
+        <div className='wrapper'>
+            <div className='main-about-wrapper'>
+                <main className='main-about'>
+                    <h2>O nas</h2>
+                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris augue, convallis id volutpat in, mattis ac eros. Donec commodo et magna id semper. Vivamus dapibus quam a placerat pellentesque. Pellentesque elementum luctus lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus.</div>
+                </main>
+            </div>
+        </div>
+    );
+}
+
+export default MainAbout;
