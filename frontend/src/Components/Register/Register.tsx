631d1247f7e7624858417970abdf2e3b8d04dc23
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-13 19:47:25 +0200

    Update Register.tsx
    created MainRegister Component

--- frontend/src/Components/Register/Register.tsx
+++ frontend/src/Components/Register/Register.tsx
@@ -1,5 +1,6 @@
 
 import NavRegister from './NavRegister';
+import MainRegister from './MainRegister';
 
 import './Register.scss';
 
@@ -9,22 +10,7 @@ function Register() {
         <>
             <div className='wrapper'>
                 <NavRegister></NavRegister>
-                <main className='main-register-wrapper'>
-                    <div className='main-register'>
-                        <h2>Zarejestruj się już teraz!</h2>
-                        <form className='register-form'>
-                            {/* <label>Adres Email</label> */}
-                            <input type='email' placeholder='Adres Email'></input>
-                            {/* <label>Nazwa użytkownika</label> */}
-                            <input type='login' placeholder='Nazwa użytkownika'></input>
-                            {/* <label>Hasło</label> */}
-                            <input type='password' placeholder='Hasło'></input>
-                            {/* <label>Hasło ponownie</label> */}
-                            <input type='password' placeholder='Hasło ponownie'></input>
-                            <input type='submit' value='Zarejestruj'></input>
-                        </form>
-                    </div>
-                </main>
+                <MainRegister></MainRegister>
             </div>
         </>
     );
