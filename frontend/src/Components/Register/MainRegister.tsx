1efec125db43edbcde1974d5738ce1604d4a6af4
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-14 16:54:13 +0200

    Update MainRegister.tsx
    named each input

--- frontend/src/Components/Register/MainRegister.tsx
+++ frontend/src/Components/Register/MainRegister.tsx
@@ -9,14 +9,14 @@ function MainRegister() {
                 <h2>Zarejestruj się już teraz!</h2>
                 <form className='register-form'>
                     {/* <label>Adres Email</label> */}
-                    <input type='email' placeholder='Adres Email'></input>
+                    <input type='email' name='email' placeholder='Adres Email'></input>
                     {/* <label>Nazwa użytkownika</label> */}
-                    <input type='login' placeholder='Nazwa użytkownika'></input>
+                    <input type='login' name='login' placeholder='Nazwa użytkownika'></input>
                     {/* <label>Hasło</label> */}
-                    <input type='password' placeholder='Hasło'></input>
+                    <input type='password' name='password' placeholder='Hasło'></input>
                     {/* <label>Hasło ponownie</label> */}
-                    <input type='password' placeholder='Hasło ponownie'></input>
-                    <input type='submit' value='Zarejestruj'></input>
+                    <input type='password' name='password-again' placeholder='Hasło ponownie'></input>
+                    <input type='submit' name='submit' value='Zarejestruj'></input>
                 </form>
             </main>
         </div>
