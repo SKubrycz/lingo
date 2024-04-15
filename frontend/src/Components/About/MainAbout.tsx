e1b38f376f59474d6f0f772662c59934abfd0b7f
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-15 07:09:09 +0200

    Update MainAbout.tsx

--- frontend/src/Components/About/MainAbout.tsx
+++ frontend/src/Components/About/MainAbout.tsx
@@ -7,7 +7,9 @@ function MainAbout() {
             <div className='main-about-wrapper'>
                 <main className='main-about'>
                     <h2>O nas</h2>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris augue, convallis id volutpat in, mattis ac eros. Donec commodo et magna id semper. Vivamus dapibus quam a placerat pellentesque. Pellentesque elementum luctus lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus.</div>
+                    <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris augue, convallis id volutpat in, mattis ac eros. Donec commodo et magna id semper. Vivamus dapibus quam a placerat pellentesque. Pellentesque elementum luctus lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus.</article>
+                    <h4>Lorem Ipsum</h4>
+                    <article>Integer lobortis consequat nibh, nec auctor elit ullamcorper congue. Morbi at blandit purus, sed consectetur felis. Vivamus efficitur nisi sit amet eros facilisis, sit amet fringilla enim porttitor. Suspendisse maximus, erat vitae sodales molestie, nulla magna aliquam magna, sed euismod libero ante eu elit.</article>
                 </main>
             </div>
         </div>
