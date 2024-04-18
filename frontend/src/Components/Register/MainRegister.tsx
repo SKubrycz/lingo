fc81a1b825416935295b6db51c29cd88925b9707
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-18 09:53:17 +0200

    Update MainRegister.tsx
    added autocomplete for inputs

--- frontend/src/Components/Register/MainRegister.tsx
+++ frontend/src/Components/Register/MainRegister.tsx
@@ -9,13 +9,13 @@ function MainRegister() {
                 <h2>Zarejestruj się już teraz!</h2>
                 <form className='register-form'>
                     {/* <label>Adres Email</label> */}
-                    <input type='email' name='email' placeholder='Adres Email'></input>
+                    <input type='email' name='email' placeholder='Adres Email' autoComplete='email'></input>
                     {/* <label>Nazwa użytkownika</label> */}
-                    <input type='login' name='login' placeholder='Nazwa użytkownika'></input>
+                    <input type='login' name='login' placeholder='Nazwa użytkownika' autoComplete='username'></input>
                     {/* <label>Hasło</label> */}
-                    <input type='password' name='password' placeholder='Hasło'></input>
+                    <input type='password' name='password' placeholder='Hasło' autoComplete='new-password'></input>
                     {/* <label>Hasło ponownie</label> */}
-                    <input type='password' name='password-again' placeholder='Hasło ponownie'></input>
+                    <input type='password' name='password-again' placeholder='Hasło ponownie' autoComplete='new-password'></input>
                     <input type='submit' name='submit' value='Zarejestruj'></input>
                 </form>
             </main>
