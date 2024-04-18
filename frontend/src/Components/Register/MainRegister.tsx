f2d3941d774d52cd57cdbb203c7fb9c4895f422a
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-18 16:33:58 +0200

    Update MainRegister.tsx
    rearranged code
    removed commented out labels

--- frontend/src/Components/Register/MainRegister.tsx
+++ frontend/src/Components/Register/MainRegister.tsx
@@ -8,15 +8,35 @@ function MainRegister() {
             <main className='main-register'>
                 <h2>Zarejestruj się już teraz!</h2>
                 <form className='register-form'>
-                    {/* <label>Adres Email</label> */}
-                    <input type='email' name='email' placeholder='Adres Email' autoComplete='email'></input>
-                    {/* <label>Nazwa użytkownika</label> */}
-                    <input type='login' name='login' placeholder='Nazwa użytkownika' autoComplete='username'></input>
-                    {/* <label>Hasło</label> */}
-                    <input type='password' name='password' placeholder='Hasło' autoComplete='new-password'></input>
-                    {/* <label>Hasło ponownie</label> */}
-                    <input type='password' name='password-again' placeholder='Hasło ponownie' autoComplete='new-password'></input>
-                    <input type='submit' name='submit' value='Zarejestruj'></input>
+                    <input 
+                        type='email' 
+                        name='email' 
+                        placeholder='Adres Email' 
+                        autoComplete='email'
+                    ></input>
+                    <input 
+                        type='login' 
+                        name='login' 
+                        placeholder='Nazwa użytkownika' 
+                        autoComplete='username'
+                    ></input>
+                    <input 
+                        type='password' 
+                        name='password' 
+                        placeholder='Hasło' 
+                        autoComplete='new-password'
+                    ></input>
+                    <input 
+                        type='password' 
+                        name='password-again' 
+                        placeholder='Hasło ponownie' 
+                        autoComplete='new-password'
+                    ></input>
+                    <input 
+                        type='submit' 
+                        name='submit' 
+                        value='Zarejestruj'
+                    ></input>
                 </form>
             </main>
         </div>
