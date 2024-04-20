b38b77c54ae5c29e056e68abf5e7fa9fdd1652c7
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-20 22:02:44 +0200

    Update MainRegister.tsx
    created axios post request to server with form credentials

--- frontend/src/Components/Register/MainRegister.tsx
+++ frontend/src/Components/Register/MainRegister.tsx
@@ -1,34 +1,111 @@
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
+import { useReducer } from 'react';
+
+type RegisterState = {
+    email: string;
+    login: string;
+    password: string;
+    passwordAgain: string;
+}
+
+enum ActionType {
+    Email = 'email',
+    Login = 'login',
+    Password = 'password',
+    PasswordAgain = 'passwordAgain',
+}
+
+interface RegisterActions {
+    type: ActionType;
+    payload?: string;
+}
+
+
+const loginReducer = (state: RegisterState, action: RegisterActions) => {
+    const { type, payload } = action;
+    switch (type) {
+        case ActionType.Email:
+            console.log(state.email);
+            return {
+                ...state,
+                email: payload || '',
+            };
+        case ActionType.Login:
+            console.log(state.login);
+            return {
+                ...state,
+                login: payload || '',
+            };
+        case ActionType.Password:
+            console.log(state.password);
+            return {
+                ...state,
+                password: payload || '',
+            };
+        case ActionType.PasswordAgain:
+            console.log(state.passwordAgain);
+            return {
+                ...state,
+                passwordAgain: payload || '',
+            }
+        default:
+            throw new Error(`No such action: ${action.type}`);
+    }
+}
 
 function MainRegister() {
-    
+    const [registerData, registerDispatch] = useReducer(loginReducer, {
+        email: '',
+        login: '',
+        password: '',
+        passwordAgain: '',
+    });
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        await axios.post('http://localhost:8000/register', registerData)
+            .then(() => {
+                navigate('/', { state: 'Rejestracja przebiegła pomyślnie' });
+            }).catch((error) => {
+                console.log(error);
+            })
+    };
 
     return (
         <div className='main-register-wrapper'>
             <main className='main-register'>
                 <h2>Zarejestruj się już teraz!</h2>
-                <form className='register-form'>
+                <form className='register-form' onSubmit={(e) => handleSubmit(e)}>
                     <input 
                         type='email' 
                         name='email' 
+                        onChange={(e) => registerDispatch({type: ActionType.Email, payload: e.target.value})}
                         placeholder='Adres Email' 
                         autoComplete='email'
                     ></input>
                     <input 
                         type='login' 
                         name='login' 
+                        onChange={(e) => registerDispatch({type: ActionType.Login, payload: e.target.value})}
                         placeholder='Nazwa użytkownika' 
                         autoComplete='username'
                     ></input>
                     <input 
                         type='password' 
-                        name='password' 
+                        name='password'
+                        onChange={(e) => registerDispatch({type: ActionType.Password, payload: e.target.value})}
                         placeholder='Hasło' 
                         autoComplete='new-password'
                     ></input>
                     <input 
                         type='password' 
                         name='password-again' 
+                        onChange={(e) => registerDispatch({type: ActionType.PasswordAgain, payload: e.target.value})}
                         placeholder='Hasło ponownie' 
                         autoComplete='new-password'
                     ></input>
