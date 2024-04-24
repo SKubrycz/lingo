b1d8a8ab24a99339e7924c032b7c9dd0d461ae1e
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-24 16:50:28 +0200

    Update NavHome.tsx

--- frontend/src/Components/Home/NavHome.tsx
+++ frontend/src/Components/Home/NavHome.tsx
@@ -4,7 +4,7 @@ function NavHome() {
     return (
         <nav className='navbar'>
             <Link to='/' className='navbar-logo'>LOGO</Link>
-            <Link to='/about'>O nas</Link>
+            <Link to='/about'>O aplikacji</Link>
             <Link to='/login'>Logowanie</Link>
             <Link to='/register'>Rejestracja</Link>
         </nav>
