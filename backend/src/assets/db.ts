663277798493241e9a82d099df845a2d49fe3d7c
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-29 10:09:42 +0200

    Update db.ts
    changed the database connection method (for now)

--- backend/src/assets/db.ts
+++ backend/src/assets/db.ts
@@ -3,27 +3,27 @@ const { MongoClient } = require("mongodb");
 const uri = 
     "mongodb://localhost:27017/language-app";
 
-const client = new MongoClient(uri, {
-});
+const client = new MongoClient(uri, {});
 
 const runDB = async () => {
     try {
         // Connect the client to the server
         await client.connect();
         // Establish and verify connection
-        const db = await client.db("language-app")
+        //const database = await client.db("language-app");
 
-        const cursor = await db.collection('users').find({ login: "Qweqwe" });
+        //const cursor = await db.collection('users').find({});
 
-        const result = await cursor.toArray();
+        //const result = await cursor.toArray();
 
-        console.log(result);
+        //console.log(result);
         console.log("Connected successfully to server");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+
+        return client;
+    } catch (err) {
+        console.log('mongodb err', err);
     }
 }
 runDB().catch(console.dir);
 
-module.exports = runDB();
+module.exports = runDB;
