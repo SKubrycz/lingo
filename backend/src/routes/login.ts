915fb054f708e5d3eba53fdc24f177d7cf2f0791
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-04-29 20:47:45 +0200

    Update login.ts
    created user check with hashed password onlogin

--- backend/src/routes/login.ts
+++ backend/src/routes/login.ts
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const queries = require('../assets/queries');
+
+const comparePassword = require('../utilities/comparePassword');
+
 router.post('/login', async (req, res) => {
     try {
         const { login, password } = await req.body;
 
         console.log(`posted in /login: ${login}, ${password}`);
 
-        res.status(200).send('Zalogowano');
+
+        const result = await queries.findOneUser(login);
+        const comparison = await comparePassword(password, result.password);
+        console.log(`comparison: ${comparison}`);
+
+        if (comparison === false) return res.status(400).send('Niepoprawne hasło');
+
+        return res.status(200).send('Zalogowano');
     } catch (error) {
         res.status(500).send(`Error /login ${error}`);
     }
