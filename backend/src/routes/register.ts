a42f7a74c5f7070100e2a05f5ac9e8e252aebf3d
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-05-01 22:17:43 +0200

    Update register.ts
    added regexp for correct password check

--- backend/src/routes/register.ts
+++ backend/src/routes/register.ts
@@ -11,10 +11,13 @@ router.post('/register', async (req, res) => {
     try {
         const { email, login, password, passwordAgain } = await req.body;
 
+        const regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
         const hash = await hashData(password);
 
         if (password === '' || !password) return res.status(404).send('Hasło niepoprawne');
         if (passwordAgain === '' || !passwordAgain) return res.status(404).send('Powtórzone hasło niepoprawne');
+        if (regex.test(password) === false) return res.status(404).send('Hasło musi być dłuższe niż 7 znaków, posiadać przynajmniej jedną dużą i małą literę, cyfrę oraz znak specjalny');
         if (password === passwordAgain) {
             console.log(`posted in /register: ${email} ${login} ${password} ${passwordAgain}, ${hash}`);
 
