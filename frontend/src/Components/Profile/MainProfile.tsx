93d7dcb53b9b3964990097d784fcd867661090c5
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-05-02 12:58:19 +0200

    Update MainProfile.tsx
    added intital statistics and overall profile layout

--- frontend/src/Components/Profile/MainProfile.tsx
+++ frontend/src/Components/Profile/MainProfile.tsx
@@ -5,10 +5,27 @@ function MainProfile() {
         <div className='main-profile-wrapper'>
             <main className='main-profile'>
                 <article className='main-profile-user'>
-                    <img src='' alt='user-img' className='profile-user-img'></img>
-                    <p>Username</p>
+                    <div className='profile-user-img'>User Img</div>
+                    <p>UsernameUsername</p>
                 </article>
-                <article className='main-profile-stats'>Yet another something</article>
+                <article className='main-profile-stats'>
+                    <div className='main-profile-stat-container'>
+                        <div>Stat1:</div>
+                        <div>123</div>
+                    </div>
+                    <div className='main-profile-stat-container'>
+                        <div>Stat2:</div>
+                        <div>238759</div>
+                    </div>
+                    <div className='main-profile-stat-container'>
+                        <div>Stat3:</div>
+                        <div>10074</div>
+                    </div>
+                    <div className='main-profile-stat-container'>
+                        <div>Stat4:</div>
+                        <div>251h</div>
+                    </div>
+                </article>
             </main>
         </div>
     );
