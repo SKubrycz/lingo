e0deb911b4bb199f1f5a727d3fcbf926e75ee464
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-05-07 09:21:59 +0200

    Update register.ts
    added a few more validation steps:
    -email and login check
    -user existence check

--- backend/src/routes/register.ts
+++ backend/src/routes/register.ts
@@ -13,6 +13,12 @@ router.post('/register', async (req, res) => {
 
         const hash = await hashData(password);
 
+        if ((email === '' || !email) || (login === '' || !login)) return res.status(422).send('Należy wypełnić wszystkie pola formularza');
+
+        const findUserQuery = await queries.findOneUser(email, login);
+        console.log(findUserQuery);
+        if (findUserQuery) return res.status(422).send('Użytkownik już istnieje');
+
         if (password === '' || !password) return res.status(422).send('Hasło niepoprawne');
         if (passwordAgain === '' || !passwordAgain) return res.status(422).send('Powtórzone hasło niepoprawne');
         if (regex.test(password) === false) return res.status(422).send('Hasło musi być dłuższe niż 7 znaków, posiadać przynajmniej jedną dużą i małą literę, cyfrę oraz znak specjalny');
