3f5b84c58fdb4d2e51106da64df723f5a29caf59
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-05-10 18:25:57 +0200

    Update Navbar.tsx
    created a simple burger menu

--- frontend/src/Components/Reusables/Navbar/Navbar.tsx
+++ frontend/src/Components/Reusables/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import { Link } from 'react-router-dom';
 
 interface NavbarProps {
@@ -6,16 +8,49 @@ interface NavbarProps {
 }
 
 function Navbar({ link, options }: NavbarProps) {
+    const [display, setDisplay] = useState<string>('none');
+
+    const navBurgerStyle: Object = {
+        display: display
+    };
+
+    const handleDisplay = () => {
+        if (display === 'none') {
+            setDisplay('flex');    
+        } else if (display === 'flex') {
+            setDisplay('none');
+        } else return;
+    }
+
+    const handleDisplayOnResize = () => {
+        if (window.innerWidth > 768) setDisplay('none');
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleDisplayOnResize)
+
+        return () => window.removeEventListener('resize', handleDisplayOnResize);
+    }, []);
 
     return (
-        <nav className='navbar'>
-            <Link to='/' className='navbar-logo'>LOGO</Link>
-            {options.map((value, index) => {
-                return (
-                    <Link key={index} to={link[index]}>{value}</Link>
-                )
-            })}
-        </nav>
+        <div className='navbar-wrapper'>
+            <nav className='navbar'>
+                <Link to='/' className='navbar-logo'>LOGO</Link>
+                {options.map((value, index) => {
+                    return (
+                        <Link key={index} to={link[index]}>{value}</Link>
+                    )
+                })}
+                <div className='navbar-burger' onClick={() => handleDisplay()}>___</div>
+            </nav>
+            <div className='navbar-burger-options' style={navBurgerStyle}>
+                {options.map((value, index) => {
+                    return (
+                        <Link key={index} to={link[index]}>{value}</Link>
+                    )
+                })}
+            </div>
+        </div>
     );
 }
 
