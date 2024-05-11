2b889d65058862aa2e4a52667d429426647e380a
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-05-11 15:05:04 +0200

    Update Lessons.tsx

--- frontend/src/Components/Lessons/Lessons.tsx
+++ frontend/src/Components/Lessons/Lessons.tsx
@@ -2,8 +2,8 @@ import Navbar from '../Reusables/Navbar/Navbar';
 
 function Lessons() {
 
-    const linkArray: string[] = ['/profile'];
-    const optionsArray: string[] = ['Profil'];
+    const linkArray: string[] = ['/about', '/profile'];
+    const optionsArray: string[] = ['O aplikacji', 'Profil'];
 
     return(
         <>
