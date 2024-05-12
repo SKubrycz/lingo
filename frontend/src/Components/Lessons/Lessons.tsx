22f918365d53a8cd9fc1d1720d21b50f2c776ca8
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-05-12 16:10:47 +0200

    Update Lessons.tsx
    api & jwt preparation

--- frontend/src/Components/Lessons/Lessons.tsx
+++ frontend/src/Components/Lessons/Lessons.tsx
@@ -1,9 +1,14 @@
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 import Navbar from '../Reusables/Navbar/Navbar';
 
 function Lessons() {
 
     const linkArray: string[] = ['/about', '/profile'];
     const optionsArray: string[] = ['O aplikacji', 'Profil'];
+
+    const navigate = useNavigate();
 
     return(
         <>
