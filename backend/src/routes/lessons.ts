80d2a1d4aff30cf3631cd08317b1bf6185e7d962
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-05-15 07:20:50 +0200

    Add files via upload

--- backend/src/routes/lessons.ts
+++ backend/src/routes/lessons.ts
@@ -8,6 +8,7 @@ const auth = require('../middleware/auth');
 
 router.get('/lessons', auth.checkAuth, async (req, res) => {
     res.send('Witamy w Lekcjach!');
-})
+});
+
 
 module.exports = router;
