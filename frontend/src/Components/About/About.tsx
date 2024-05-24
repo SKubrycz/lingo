4e33a9b201e7848708dbe324027e80e4c20def97
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-05-24 12:15:18 +0200

    Update About.tsx
    added data fetch to change the navbar according to whether the user is logged in or not

--- frontend/src/Components/About/About.tsx
+++ frontend/src/Components/About/About.tsx
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 import Navbar from '../Reusables/Navbar/Navbar';
 import MainAbout from './MainAbout';
 
@@ -5,8 +8,25 @@ import './About.scss';
 
 function About() {
 
-    const linkArray: string[] = ['/login', '/register'];
-    const optionsArray: string[] = ['Logowanie', 'Rejestracja'];
+    const [linkArray, setLinkArray] = useState<string[]>(['/login', '/register']);
+    const [optionsArray, setOptionsArray] = useState<string[]>(['Logowanie', 'Rejestracja']);
+
+    const handleAuth = () => {
+        axios.get('http://localhost:8000/about', { withCredentials: true })
+            .then((res) => {
+                console.log(res.data.login);
+                if (res.data.login) {
+                    setLinkArray(['/lessons', `/profile/${res.data.login}`]);
+                    setOptionsArray(['Lekcje', 'Profil']);
+                }
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+
+    useEffect(() => {
+        handleAuth();
+    }, [])
 
     return (
         <>
