821b7a9db29f1290874d0e10034e64ff1b8c230c
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-05-25 09:34:28 +0200

    Update About.tsx

--- frontend/src/Components/About/About.tsx
+++ frontend/src/Components/About/About.tsx
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 import Navbar from '../Reusables/Navbar/Navbar';
 import MainAbout from './MainAbout';
+
+import { useMessage } from '../..';
 
 import './About.scss';
 
@@ -11,16 +14,24 @@ function About() {
     const [linkArray, setLinkArray] = useState<string[]>(['/login', '/register']);
     const [optionsArray, setOptionsArray] = useState<string[]>(['Logowanie', 'Rejestracja']);
 
+    const { message, setMessage } = useMessage();
+
+    const navigate = useNavigate();
+
     const handleAuth = () => {
         axios.get('http://localhost:8000/about', { withCredentials: true })
             .then((res) => {
-                console.log(res.data.login);
                 if (res.data.login) {
+                    console.log(res.data.login);
                     setLinkArray(['/lessons', `/profile/${res.data.login}`]);
                     setOptionsArray(['Lekcje', 'Profil']);
+                } else {
+                    console.log(res.data);
                 }
             }).catch((error) => {
                 console.log(error);
+                setMessage('Sesja wygasła. Proszę zalogować się ponownie');
+                navigate('/');
             })
     }
 
