4cc696ee5a58c4bbb86b7a5b7e124ef8acd96cfb
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-06-04 19:07:45 +0200

    Update MainProfile.tsx
    moved route get requests to the parent component

--- frontend/src/Components/Profile/MainProfile.tsx
+++ frontend/src/Components/Profile/MainProfile.tsx
@@ -1,57 +1,9 @@
-import axios from 'axios';
-
-import { useParams, useNavigate } from 'react-router-dom';
-
-import { useEffect, useState } from 'react';
-
 interface User {
     login: string;
     sessionUser: boolean;
 }
 
-function MainProfile() {
-    const { userId } = useParams<{ userId: string }>();
-    const [user, setUser] = useState<User | null>(null);
-
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const fetchCurrentUser = async () => {
-            try {
-                const res = await axios.get(`http://localhost:8000/profile`, { withCredentials: true });
-                console.log(res.data);
-                /* if (res.data.sessionUser === true) {
-                    navigate(`/profile/${res.data.login}`);
-                } else {
-                    navigate(`/profile/${userId}`);
-                } */
-            } catch (error) {
-                console.log('Failed to fetch current user data: ', error);
-                navigate('/');
-            }
-        }
-
-        fetchCurrentUser();
-
-        const fetchUserData = async () => {
-          try {
-            const res = await axios.get<User>(`http://localhost:8000/profile/${userId}`, { withCredentials: true });
-            console.log(res.data);
-            if (res.data.sessionUser === true) {
-                console.log('sessionUser: ' + res.data.sessionUser);
-                setUser(res.data);
-            } else {
-                setUser(res.data);
-            }
-          } catch (error) {
-            console.error("Failed to fetch user data:", error);
-            navigate('/');
-          }
-        };
-    
-        fetchUserData();
-        
-     }, [userId]);
+function MainProfile({user}: {user: User | null}) {
 
     return (
         <div className='main-profile-wrapper'>
