0a569473e99ed4bfd95f4c2a0382a0bed729effc
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-06-08 16:39:33 +0200

    Update handleInputVisibility.ts
    added e.target

--- frontend/src/Components/Reusables/Functions/handleInputVisibility.ts
+++ frontend/src/Components/Reusables/Functions/handleInputVisibility.ts
@@ -1,9 +1,12 @@
-const handleInputVisibility = (inputRef: React.RefObject<HTMLInputElement>) => {
+const handleInputVisibility = (e: React.MouseEvent<HTMLDivElement>, inputRef: React.RefObject<HTMLInputElement>) => {
     if (inputRef && inputRef.current) {
+        const target = e.target as HTMLElement;
         if (inputRef.current.type === 'password') {
             inputRef.current.type = 'text';
+            target.textContent = 'Ukryj';
         } else if (inputRef.current.type === 'text') {
             inputRef.current.type = 'password';
+            target.textContent = 'Pokaż';
         }
     }
 }
