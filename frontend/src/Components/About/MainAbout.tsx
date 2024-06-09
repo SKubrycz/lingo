5c1420621b7e311bdb8f6a7198882604c9b99857
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-06-09 11:53:02 +0200

    Update MainAbout.tsx

--- frontend/src/Components/About/MainAbout.tsx
+++ frontend/src/Components/About/MainAbout.tsx
@@ -3,17 +3,18 @@
 function MainAbout() {
 
     return (
-        
         <div className='main-about-wrapper'>
             <main className='main-about'>
-                <h2>O aplikacji</h2>
-                <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris augue, convallis id volutpat in, mattis ac eros. Donec commodo et magna id semper. Vivamus dapibus quam a placerat pellentesque. Pellentesque elementum luctus lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus.</article>
-                <h4>Lorem Ipsum</h4>
-                <article>Integer lobortis consequat nibh, nec auctor elit ullamcorper congue. Morbi at blandit purus, sed consectetur felis. Vivamus efficitur nisi sit amet eros facilisis, sit amet fringilla enim porttitor. Suspendisse maximus, erat vitae sodales molestie, nulla magna aliquam magna, sed euismod libero ante eu elit.</article>
-                <h4>Dolor sit amet</h4>
-                <article>Proin erat nibh, semper ut rhoncus eu, vulputate in est. Mauris accumsan tincidunt leo quis vulputate. Pellentesque posuere condimentum sem sed iaculis. Sed in lectus at eros accumsan vestibulum. Etiam pellentesque nibh lectus, eget imperdiet massa tristique sed.</article>
-                <h4>Curabitur a venenatis ipsum</h4>
-                <article>Suspendisse nec lorem finibus, fermentum ipsum sed, iaculis orci. Praesent vitae feugiat turpis, in bibendum purus. Integer sodales, ex ac congue sagittis, neque tellus convallis mi, ut mattis lorem orci sed velit. Morbi tristique sagittis nulla eu tristique. Aliquam facilisis ipsum vel lacus dignissim, sed sollicitudin arcu efficitur. Nulla eget lacus in risus maximus ultricies vitae nec justo. Curabitur tristique massa ac nibh pretium, sed viverra arcu facilisis. Quisque ac ultricies ipsum. Nulla sit amet libero quis nulla molestie molestie nec sed tortor.</article>
+                <div className='main-about-content'>
+                    <h2>O aplikacji</h2>
+                    <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris augue, convallis id volutpat in, mattis ac eros. Donec commodo et magna id semper. Vivamus dapibus quam a placerat pellentesque. Pellentesque elementum luctus lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus.</article>
+                    <h4>Lorem Ipsum</h4>
+                    <article>Integer lobortis consequat nibh, nec auctor elit ullamcorper congue. Morbi at blandit purus, sed consectetur felis. Vivamus efficitur nisi sit amet eros facilisis, sit amet fringilla enim porttitor. Suspendisse maximus, erat vitae sodales molestie, nulla magna aliquam magna, sed euismod libero ante eu elit.</article>
+                    <h4>Dolor sit amet</h4>
+                    <article>Proin erat nibh, semper ut rhoncus eu, vulputate in est. Mauris accumsan tincidunt leo quis vulputate. Pellentesque posuere condimentum sem sed iaculis. Sed in lectus at eros accumsan vestibulum. Etiam pellentesque nibh lectus, eget imperdiet massa tristique sed.</article>
+                    <h4>Curabitur a venenatis ipsum</h4>
+                    <article>Suspendisse nec lorem finibus, fermentum ipsum sed, iaculis orci. Praesent vitae feugiat turpis, in bibendum purus. Integer sodales, ex ac congue sagittis, neque tellus convallis mi, ut mattis lorem orci sed velit. Morbi tristique sagittis nulla eu tristique. Aliquam facilisis ipsum vel lacus dignissim, sed sollicitudin arcu efficitur. Nulla eget lacus in risus maximus ultricies vitae nec justo. Curabitur tristique massa ac nibh pretium, sed viverra arcu facilisis. Quisque ac ultricies ipsum. Nulla sit amet libero quis nulla molestie molestie nec sed tortor.</article>
+                </div>
             </main>
         </div>
     );
