4eaa99ba9c6e53a84256ed5f4f6b4107aa571520
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-06-10 05:44:46 +0200

    Update Home.tsx
    added footer

--- frontend/src/Components/Home/Home.tsx
+++ frontend/src/Components/Home/Home.tsx
@@ -2,6 +2,7 @@
 
 import Navbar from '../Reusables/Navbar/Navbar';
 import MainHome from './MainHome';
+import Footer from '../Reusables/Footer/Footer';
 
 import './Home.scss';
 
@@ -9,11 +10,15 @@ function Home() {
 
   const linkArray: string[] = ['/about', '/login', '/register'];
   const optionsArray: string[] = ['O aplikacji', 'Logowanie', 'Rejestracja'];
+
+  const footerLinkArray: string[] = ['/about', '/login', '/register'];
+  const footerOptionsArray: string[] = ['O aplikacji', 'Logowanie', 'Rejestracja'];
   
   return (
     <div className='wrapper'>
       <Navbar link={linkArray} options={optionsArray}></Navbar>
       <MainHome></MainHome>
+      <Footer link={footerLinkArray} options={footerOptionsArray}></Footer>
     </div>
   );
 }
