9eb5ed7caa999585a7d34ee840db1239ee6fa23d
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-06-17 07:57:55 +0200

    Update Login.tsx
    added footer

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -1,5 +1,6 @@
 import Navbar from '../Reusables/Navbar/Navbar';
 import MainLogin from './MainLogin';
+import Footer from '../Reusables/Footer/Footer';
 
 import './Login.scss';
 
@@ -9,11 +10,15 @@ function Login() {
     const linkArray: string[] = ['/about', '/register'];
     const optionsArray: string[] = ['O aplikacji', 'Rejestracja'];
 
+    const footerLinkArray: string[] = ['/about', '/login', '/register'];
+    const footerOptionsArray: string[] = ['O aplikacji', 'Logowanie', 'Rejestracja'];
+
     return (
         <>
             <div className='wrapper'>
                 <Navbar link={linkArray} options={optionsArray}></Navbar>
                 <MainLogin></MainLogin>
+                <Footer link={footerLinkArray} options={footerOptionsArray}></Footer>
             </div>
         </>
     );
