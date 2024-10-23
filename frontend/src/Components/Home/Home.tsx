caac6bd3be0cc999ced4cfb5b8dc2722e075a84b
DESKTOP-9GVJBJP\Reishi <sebastian.kubrycz@gmail.com>
2024-10-23 10:32:16 +0200

    feature: Added scrolling arrow

--- frontend/src/Components/Home/Home.tsx
+++ frontend/src/Components/Home/Home.tsx
@@ -1,23 +1,54 @@
-//import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 
-import Navbar from '../Reusables/Navbar/Navbar';
-import MainHome from './MainHome';
-import Footer from '../Reusables/Footer/Footer';
+import Navbar from "../Reusables/Navbar/Navbar";
+import MainHome from "./MainHome";
+import Footer from "../Reusables/Footer/Footer";
 
-import './Home.scss';
+import "./Home.scss";
+import ScrollArrow from "../Reusables/ScrollArrow/ScrollArrow";
 
 function Home() {
+  const linkArray: string[] = ["/about", "/login", "/register"];
+  const optionsArray: string[] = ["O aplikacji", "Logowanie", "Rejestracja"];
 
-  const linkArray: string[] = ['/about', '/login', '/register'];
-  const optionsArray: string[] = ['O aplikacji', 'Logowanie', 'Rejestracja'];
+  const footerLinkArray: string[] = ["/about", "/login", "/register"];
+  const footerOptionsArray: string[] = [
+    "O aplikacji",
+    "Logowanie",
+    "Rejestracja",
+  ];
 
-  const footerLinkArray: string[] = ['/about', '/login', '/register'];
-  const footerOptionsArray: string[] = ['O aplikacji', 'Logowanie', 'Rejestracja'];
-  
+  const [visible, setVisible] = useState<boolean>(true);
+
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const checkScroll = () => {
+    if (document.scrollingElement) {
+      const { scrollHeight, clientHeight, scrollTop } =
+        document.scrollingElement;
+
+      if (Math.abs(scrollHeight - (scrollTop + clientHeight)) <= 1) {
+        setVisible(false);
+      } else {
+        setVisible(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("scroll", checkScroll);
+    console.log("after adding");
+
+    return () => {
+      document.removeEventListener("scroll", checkScroll);
+    };
+  }, []);
+
   return (
-    <div className='wrapper'>
+    <div ref={wrapperRef} className="wrapper">
       <Navbar link={linkArray} options={optionsArray}></Navbar>
       <MainHome></MainHome>
+      {visible && <ScrollArrow></ScrollArrow>}
       <Footer link={footerLinkArray} options={footerOptionsArray}></Footer>
     </div>
   );
